9ee71d621c5a86dabad1ea41229e4616e76c68a8
flightman69 <flightman69420@gmail.com>
2024-10-29 17:34:43 +0530

    refined 3.b code

--- advent_of_code_2021/day_3.b/main.rs
+++ advent_of_code_2021/day_3.b/main.rs
@@ -1,20 +1,21 @@
 fn main() {
     let file = include_str!("./input.txt");
-    let input_bits: Vec<&str> = file.lines().collect();
+    let input_bits: Vec<&str> = file.lines().collect(); // array of bits as strings
 
-    let oxygen_meter = get_oxygen(&input_bits, 0 as usize);
-    let co2_meter = get_co2(&input_bits, 0 as usize);
+    let oxygen_meter = get_meter_bit(&input_bits, 0 as usize, "oxygen");
+    let co2_meter = get_meter_bit(&input_bits, 0 as usize, "co2");
 
     let life_possibility = oxygen_meter * co2_meter;
     println!("{}", life_possibility);
 }
 
-fn get_oxygen(inputs: &Vec<&str>, index: usize) -> i32 {
+fn get_meter_bit(inputs: &Vec<&str>, index: usize, which_meter: &str) -> i32 {
     if inputs.len() == 1 {
-        return bin_2_dec(inputs[0]);
+        return i32::from_str_radix(inputs[0], 2).expect("oops!"); //return the final bits in decimal
     }
-    let mut one_bits: Vec<&str> = vec![];
-    let mut zero_bits: Vec<&str> = vec![];
+
+    let mut one_bits: Vec<&str> = vec![]; // array for 1 bit
+    let mut zero_bits: Vec<&str> = vec![]; // array for 0 bit
 
     for bit in inputs.iter() {
         match bit.chars().nth(index) {
@@ -23,40 +24,19 @@ fn get_oxygen(inputs: &Vec<&str>, index: usize) -> i32 {
             _ => {}
         };
     }
-    if zero_bits.len() > one_bits.len() {
-        get_oxygen(&zero_bits, (index + 1) as usize)
+
+    // recurrsion babyy
+    if which_meter == "oxygen" {
+        if zero_bits.len() > one_bits.len() {
+            get_meter_bit(&zero_bits, (index + 1) as usize, "oxygen")
+        } else {
+            get_meter_bit(&one_bits, (index + 1) as usize, "oxygen")
+        }
     } else {
-        get_oxygen(&one_bits, (index + 1) as usize)
-    }
-}
-
-fn get_co2(inputs: &Vec<&str>, index: usize) -> i32 {
-    if inputs.len() == 1 {
-        return bin_2_dec(inputs[0]);
-    }
-    let mut one_bits: Vec<&str> = vec![];
-    let mut zero_bits: Vec<&str> = vec![];
-
-    for bit in inputs.iter() {
-        match bit.chars().nth(index) {
-            Some('0') => zero_bits.push(bit),
-            Some('1') => one_bits.push(bit),
-            _ => {}
-        };
-    }
-    if zero_bits.len() > one_bits.len() {
-        get_co2(&one_bits, (index + 1) as usize)
-    } else {
-        get_co2(&zero_bits, (index + 1) as usize)
-    }
-}
-
-fn bin_2_dec(string: &str) -> i32 {
-    let mut decimal = 0;
-    for (i, bit) in string.chars().rev().enumerate() {
-        if bit == '1' {
-            decimal += 1 << i;
+        if zero_bits.len() > one_bits.len() {
+            get_meter_bit(&one_bits, (index + 1) as usize, "co2")
+        } else {
+            get_meter_bit(&zero_bits, (index + 1) as usize, "co2")
         }
     }
-    decimal
 }
